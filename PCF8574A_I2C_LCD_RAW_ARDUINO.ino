9bd6fdf8fb25f7b0457ba34ba322d2226bbef124
unknown <brillosolutions@gmail.com>
2020-08-03 13:38:55 +0530

    Backlight fix & Method overloading

--- PCF8574A_I2C_LCD_RAW_ARDUINO.ino
+++ PCF8574A_I2C_LCD_RAW_ARDUINO.ino
@@ -1,17 +1,20 @@
 #include <Wire.h>
 #define DEV_ADDR 0x3F // I2C address of PCF8574A extender
 
+byte backLight;
+
 void setup() 
 {
   Wire.begin();
+  setBackLight(true);
   initDisplay();
 }
 
 void loop() 
 {
-  showData("Hello World", 1, 3, true);  // Data, Row Index, Cursor Index, Backlight Condition
-  showData("Brillo Solutions", 2, 1, true);  // Data, Row Index, Cursor Index, Backlight Condition
-  while (1);
+  showData("Hello World", 1, 3);  // String Data, Row Index, Cursor Index
+  showData('.', 1, 14);  // Char Data, Row Index, Cursor Index
+  showData("Brillo Solutions", 2, 1);  // Data, Row Index, Cursor Index
 }
 
 void sendToDisplay(byte mByte)  // Methods sending 8-bit data to PCF8574A I2C extender
@@ -25,26 +28,26 @@ void brkInstByte(byte mByte)
 {
   byte nByte;
   nByte = mByte & 0xF0;
-  sendToDisplay(nByte | 0x04);
+  sendToDisplay(nByte | (backLight | 0x04));
   delay(1);
-  sendToDisplay(nByte | 0x00);
+  sendToDisplay(nByte | (backLight | 0x00));
   nByte = ((mByte << 4) & 0xF0);
-  sendToDisplay(nByte | 0x04);
+  sendToDisplay(nByte | (backLight | 0x04));
   delay(1);
-  sendToDisplay(nByte | 0x00);
+  sendToDisplay(nByte | (backLight | 0x00));
 }
 
 void brkDataByte(byte mByte)
 {
   byte nByte;
   nByte = mByte & 0xF0;
-  sendToDisplay(nByte | 0x05);
+  sendToDisplay(nByte | (backLight | 0x05));
   delay(1);
-  sendToDisplay(nByte | 0x01);
+  sendToDisplay(nByte | (backLight | 0x01));
   nByte = ((mByte << 4) & 0xF0);
-  sendToDisplay(nByte | 0x05);
+  sendToDisplay(nByte | (backLight | 0x05));
   delay(1);
-  sendToDisplay(nByte | 0x01);
+  sendToDisplay(nByte | (backLight | 0x01));
 }
 
 void initDisplay()
@@ -54,7 +57,7 @@ void initDisplay()
     brkInstByte(mArr[k]);  // Send 8-bit LCD commands in 4-bit mode
 }
 
-void showData(const char *p, byte rn, byte cp, boolean bl)
+void showData(const char *p, byte rn, byte cp)
 {
   byte rowAddr;
   
@@ -70,14 +73,25 @@ void showData(const char *p, byte rn, byte cp, boolean bl)
     brkDataByte(*p);  // Send 8-bit LCD data in 4-bit mode
     p++;
   }
+}
 
-  setBackLight(bl);
+void showData(byte mByte, byte rn, byte cp)
+{
+  byte rowAddr;
+  
+  if (rn == 1)
+    rowAddr = 0x80; // First row of 16x2 LCD
+  else
+    rowAddr = 0xC0; // Second row of 16x2 LCD
+
+  brkInstByte(rowAddr + (cp - 1));  // Send 8-bit LCD commands in 4-bit mode 
+  brkDataByte(mByte);  // Send 8-bit LCD data in 4-bit mode
 }
 
 void setBackLight(boolean mBool)
 {
   if (mBool)
-    sendToDisplay(0xFF);  // Turn ON backlight of LCD
+    backLight = 0x08;  // Turn ON backlight of LCD
   else
-    sendToDisplay(0xF7);  // Turn OFF backlight of LCD
+    backLight = 0x00;  // Turn OFF backlight of LCD
 }
